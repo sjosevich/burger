5b7c97828dc31bba0b17ad5d7cb3f9fa918a64ab
Serguei Rodriguez <sjosevich@gmail.com>
2020-02-22 15:10:41 -0700

    Changin params in connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.JAWSDB_HOST,
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "burgers_db"
+  database: process.env.JAWSDB_DB
 
 } || process.env.JAWSDB_URL);
 
